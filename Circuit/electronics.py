7af6f0e67421da9fb35f7bac36a2cf8a6d6ad606
Berserker66 <fabian.dill@web.de>
2014-05-25 10:09:06 +0200

    Basic Circuit Board Implementation

--- Circuit/electronics.py
+++ Circuit/electronics.py
@@ -2,6 +2,14 @@
 import pygame as P
 from random import randint
 
+
+
+if __name__ == "__main__":
+    import sys
+    sys.path.append("..")
+from Circuit import paths
+from Engine.effects import repeated_surface as repeat
+
 class Chip():
     def __init__(self, length, connector, innercolor = P.Color(50,50,50), deviation = 3, bordercolor = P.Color(150,150,150)):
         size = length,length
@@ -25,10 +33,10 @@ class Chip():
             print("Warning|electronics.py:could not center chip connectors, change chip size.")
             
         con1,con2,con3,con4 = connector.surfaces
-        posses = tuple(range(rest//2, innerlength-rest//2, ele))
+        posses = tuple(range(connector.indent+rest//2, connector.indent+innerlength-rest//2, ele))
         y = length-connector.length
         for x in posses:
-            mx = connector.indent+x
+            mx = x
             
             self.surface.blit(con2,(mx,0))#top
             self.surface.blit(con4,(mx,y))#bottom
@@ -40,15 +48,70 @@ class Chip():
     
         
 class Grid():
-    def __init__(self, size, chip, connector, positions):
+    def __init__(self, size, chip, connector, positions, tilemap):
         self.size = size
         self.chip = chip
         self.length = connector.width+connector.spacing
         self.surface = P.Surface(size)
+        self.surface.fill(tilemap.basecolor)
         xshift = self.chip.surface.get_width()//2
+        self.chipposs = []
+        levels = set()
+        rows = set()
+        barrows = []
+        barlines = []
         for x,y in positions:
-            self.surface.blit(chip.surface, (x-xshift, y-xshift))
+            x,y = pos = (x-xshift, y-xshift)
+            self.chipposs.append(pos)
+            if y not in levels:
+                levels.add(y)
+                barlines += (y+interface for interface in chip.interfaces)
+            if x not in rows:
+                rows.add(x)
+                barrows += (x+interface for interface in chip.interfaces)
+        bar = repeat(tilemap["h"], (size[0], 5))
+        for y in barlines:
+            self.surface.blit(bar, (0, y-1))
+        bar = repeat(tilemap["v"], (5, size[1]))
+        for x in barlines:
+            self.surface.blit(bar, (x-1, 0))
+        [self.surface.blit(chip.surface, pos) for pos in self.chipposs]
+        
+    
+class TileMap():
+    def __init__(self, outercolor = (0,150,0), innercolor = (250,250,250)):
+        
+        middlecolor = P.Color(*[(x+y)//2 for x,y in zip(innercolor, outercolor)])
+        outercolor = P.Color(*outercolor)
+        innercolor = P.Color(*innercolor)
+        self.basecolor = outercolor
+        size = 5,5
+        l = 5
+        self.tiles = {}
+        #shortcuts
+        m = middlecolor
+        o = outercolor
+        i = innercolor
 
+
+        S = P.Surface(size)
+        PA = P.PixelArray(S)
+        for x,color in zip(range(5), (o,m,i,m,o)):
+            PA[x] = color
+        self.tiles["v"] = S#vertical
+        
+        S = P.Surface(size)
+        PA = P.PixelArray(S)
+        for x,color in zip(range(5), (o,m,i,m,o)):
+            PA[:, x] = color
+        self.tiles["h"] = S#horizontal
+    def __getitem__(self, key):
+        return self.tiles[key]
+        
+    def save_images(self):
+        for name, surface in self.tiles.items():
+            P.image.save(surface, name+".png")
+    
 def create_conductor(length, width, light = (200,200,200), dark = (127,127,127)):
     l = length//3
     T = P.Surface((length, width))
@@ -75,6 +138,7 @@ class Connector():
         self.surfaces = get_rotated_conductors(length, width, light, dark)
 
 if __name__ == "__main__":
+    
     P.init()
     connectors = Connector(3,10,3)
     chip = Chip(78, connectors)
@@ -87,5 +151,7 @@ if __name__ == "__main__":
         for y in range(xdelta,size[1],xdelta):
             print(x,y)
             positions.append((x,y))
-    grid = Grid(size, chip, connectors, positions)
+    tilemap = TileMap()
+    tilemap.save_images()
+    grid = Grid(size, chip, connectors, positions, tilemap)
     P.image.save(grid.surface, "test.png")
