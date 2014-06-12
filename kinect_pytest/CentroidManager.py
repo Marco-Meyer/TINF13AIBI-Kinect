1cff4123eb9d0d1d8e665c9455c3a9943f9bc920
Prenglis (Jonas E.) <j.e@vollbio.de>
2014-06-12 17:04:45 +0000

    Bercehnung vom Schwerpunkt und von Bewegung :)=

---
+++ kinect_pytest/CentroidManager.py
@@ -0,0 +1,74 @@
+import numpy as np
+import freenect
+import pygame as pygame
+import time
+
+pygame.init()
+
+class CentroidManager:
+
+	def __init__(self, initialImage, size):
+		self._size = size
+		self._initialImage = initialImage
+
+	def getCentroid(self, currentImage):
+		return self._getCentroid(self._getDeltaImage(currentImage))
+
+	def _getCentroid(self, surface):
+		pMask = pygame.mask.from_threshold(surface, (255,255,255,255), (255,255,255,255))
+		return pMask.centroid()
+        
+	def _getDeltaImage(self, currentImage):
+		return self._getPyGameSurface(self._initialImage - currentImage)
+
+	def _getPyGameSurface(self, depthImage):
+		sface = pygame.Surface(self._size)
+		arr = pygame.surfarray.pixels_green(sface)
+		arr[:] = depthImage[:]
+		arr = pygame.surfarray.pixels_red(sface)
+		arr[:] = depthImage[:]
+		arr = pygame.surfarray.pixels_blue(sface)
+		arr[:] = depthImage[:]
+		del(arr)
+		
+		pxArray = pygame.PixelArray(sface)
+		pxArray.replace(pygame.Color(255,255,255,255), pygame.Color(0,0,0,255), 0.2)
+		pxArray.replace(pygame.Color(255,255,255,255), pygame.Color(255,255,255,255), 0.9)
+		del(pxArray)
+
+		return pygame.transform.rotate(sface,-90)   
+
+def getDepthMap():    
+    depth, timestamp = freenect.sync_get_depth()
+ 
+    np.clip(depth, 0, 2**10 - 1, depth)
+    depth >>= 2
+    depth = depth.astype(np.uint8)
+ 
+    return depth
+
+if(__name__ == "__main__"):
+	initMap = getDepthMap()
+	centroidManager = CentroidManager(initMap, (480, 640))
+
+	while 1:
+		time.sleep(0.1)
+		for e in pygame.event.get():
+			if e.type == pygame.QUIT:
+				break
+
+		display = pygame.display.set_mode((640,480))
+
+		draw = centroidManager._getDeltaImage(getDepthMap())
+		pygame.draw.circle(draw, pygame.Color(255,0,0,255), centroidManager.getCentroid(getDepthMap()), 10)
+		display.blit(draw, (0,0))
+		
+		#display.blit(centroidManager._getPyGameSurface(getDepthMap()), (320,0))
+		
+		#display.blit(centroidManager._getPyGameSurface(initMap), (0,240))
+
+		pygame.display.update()
+		print("Rendering")
+
+
+
---
+++ kinect_pytest/DeltaManager.py
@@ -0,0 +1,70 @@
+class DeltaManager:
+	def __init__(self, delta):
+		self._centroids = []
+		self._delta = delta
+		self._lastMovement = -1	
+
+	def addCentroid(self, centroid):
+		self._centroids.append(centroid)
+
+	def _findMovement(self):
+		start = self._centroids[0]
+		nMove = -1
+		for x in self._centroids[:0:-1]:
+			pMove = _isMovement(start, x)
+			if(pMove != -1):
+				nMove = pMove
+				break;
+		
+		self._centroids = []
+		self._lastMovement = nMove
+		return nMove
+
+
+	"""
+	-1 - Keine Bewegung
+	0 - Rechts
+	1 - Links
+	2 - Hoch
+	3 - Runter
+	"""
+	def _isMovement(self, a, b):
+		x1, y1 = a
+		x2, y2 = b
+		
+		if self._calculateDelta(0) < abs(x1-x2) and x1 < x2:
+			return 0
+		elif self._calculateDelta(1) < abs(x1-x2) and x1 > x2:
+			return 1
+		elif self._calculateDelta(2) < abs(y1-y2) and y1 < y2:
+			return 2
+		elif self._calculateDelta(3) < abs(y1-y2) and y1 > y2: 
+			return 3
+		else:
+			return -1
+	
+	
+	def _calculateDelta(self, direction)
+		if direction == 0 && self._lastMovement == 1:
+			return self._delta * 2
+		elif direction == 1 && self._lastMovement == 0:
+			return self._delta * 2
+		elif direction == 2 && self._lastMovement == 3:
+			return self._delta * 2
+		elif direction == 3 && self._lastMovement == 2:
+			return self._delta * 2
+
+		return self._delta
+			
+		
+
+if(__name__ == "__main__"):
+	delta = DeltaManager(2)
+
+	delta.addCentroid((0,0))
+	delta.addCentroid((0,1))
+	delta.addCentroid((0,2))
+	delta.addCentroid((1,0))
+	delta.addCentroid((2,0))
+
+	delta._findMovement()
