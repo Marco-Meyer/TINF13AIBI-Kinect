46513d808226c2c942d53a2eca736cf720e4d96f
iwan933 <johannboschmann@hotmail.de>
2014-06-20 11:02:40 +0200

    fixes
    changed for pygame events

--- Engine/event_main.py
+++ Engine/event_main.py
@@ -5,15 +5,13 @@ import time
 import centroid_manager
 import delta_manager
 
-pygame.init()
-
 """
 Settings
 """
 delta           = 10            #Delta movement (in px)
 delta_interval  = 2.0           #Movement check interval (in s)
 kinect_interval = 0.2           #Kinect refresh interval (in s)
-kinect_crop     = 20            #cropped border size
+kinect_crop     = 20            #cropped border size (in px)
 
 def main(eventManager)
     centroidManager = MaskCentroidManager(kinect_crop)
@@ -31,7 +29,9 @@ def main(eventManager)
         
         if time_passed >= delta_interval:
             #push event
-            eventManager.dispatch(*deltaManager.get_Move_Events())
+            #eventManager.dispatch(*deltaManager.get_Move_Events())
+            dic = { 0 : pygame.K_RIGHT, 2 : pygame.K_LEFT, 1 : pygame.K_TOP, 3 : pygame.K_BOTTOM }
+            pygame.event.Event(pygame.KEYDOWN, dic[deltaManager.get_Move_Events()])
             time_passed = 0.0
 
         
